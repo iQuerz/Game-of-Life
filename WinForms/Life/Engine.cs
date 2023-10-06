56e1fe626def29afb76f88da5eeed9b6371ed525
iQuerz <niki.tenis@gmail.com>
2023-10-06 22:21:43 +0200

    Idk not finished

---
+++ WinForms/Life/Engine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life
+{
+    internal class Engine
+    {
+        private int _squareSize { get; set; }
+        private int _width, _height;
+        private Color _squareColor { get; set; }
+        private Color _backgrounColor { get; set; }
+
+        private bool[,] _matrix;
+
+        public Engine(PictureBox pictureBox, int squareSize)
+        {
+            _squareSize = squareSize;
+            _width = pictureBox.Width / squareSize;
+            _height = pictureBox.Height / squareSize;
+            Generate();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Generate()
+        {
+            Random random = new Random(Convert.ToInt32(DateTime.UtcNow.ToFileTimeUtc()));
+            for (int i = 0; i < _height; i++)
+                for (int j = 0; j < _width; j++)
+                    _matrix[i, j] = random.Next(0, 100) > 30;
+        }
+
+        public void Stop()
+        {
+
+        }
+
+
+        private void NextStep()
+        {
+            
+        }
+    }
+}
---
+++ WinForms/Life/Life.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Life
+{
+    partial class Life
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Location = new System.Drawing.Point(12, 41);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(776, 397);
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(12, 12);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(75, 23);
+            this.PlayButton.TabIndex = 1;
+            this.PlayButton.Text = "Start";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(93, 12);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(174, 12);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 3;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            // 
+            // Life
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.MainPictureBox);
+            this.Name = "Life";
+            this.Text = "Life";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox MainPictureBox;
+        private Button PlayButton;
+        private Button StopButton;
+        private Button ResetButton;
+    }
+}
